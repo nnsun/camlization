c3cac0436c7d93af435a25cdcc36a7269187bb26
Daniel Li <dl743@cornell.edu>
2017-11-14 19:40:20 -0500

    Add to player, state, tech

--- game/player.mli
+++ game/player.mli
@@ -1,1 +1,20 @@
 type player
+
+(* Amount of gold a player has stored *)
+val gold : player -> int
+
+(* Amount of gold a player is gaining per turn. Can be negative *)
+val gold_rate : player -> int
+
+(* Amount of points a player has accumulated thus far *)
+val points : player -> int
+
+(* Science the player has accumulated thus far *)
+val science : player -> int
+
+(* Science the player accumulates per turn *)
+val science_rate : player -> int
+
+(* Technologies the player has researched *)
+val techs : player -> Tech.tech list
+
--- game/state.mli
+++ game/state.mli
@@ -1,1 +1,9 @@
 type state
+
+type time = int
+
+(* The amount of time in years surpassed in the game so far *)
+val date : state -> time
+
+(* The map of the game *)
+val map : state -> Map.map
