b972150eedef0e337d5fd2b099bf9e19514006ea
Daniel Li <dl743@cornell.edu>
2017-12-03 17:48:04 -0500

    Remove old code

--- gui.ml
+++ gui.ml
@@ -3,7 +3,6 @@ open Notty_unix
 open Notty_helper
 open State
 
-let gui_height = 8
 let left_padding = 2
 let top_padding = 1
 let right_padding = 2
@@ -32,27 +31,6 @@ let ui_img (w, h) gst =
     gold_rate
   ] in
   I.(metrics </> I.tile w 1 (I.string A.(bg black) " "))
-
-let outline attr t =
-  let (w, h) = Term.size t in
-  let chr x = I.uchar attr x 1 1
-  and hbar = I.uchar attr 0x2500 (w - 2) 1
-  and bottom_vbar = I.uchar attr 0x2502 1 (gui_height)
-  and top_vbar = I.uchar attr 0x2502 1 (h - 3 - gui_height) in
-  let (a, b, c, d) = (chr 0x256d, chr 0x256e, chr 0x256f, chr 0x2570) in
-  grid [
-    [a; hbar; b];
-    [top_vbar; I.void (w - 2) 1; top_vbar];
-    [chr 0x2502; hbar; chr 0x2502];
-    [bottom_vbar; I.void (w - 2) 1; bottom_vbar];
-    [d; hbar; c]
-  ]
-
-let size_box (cols, rows) =
-  let cols_str = string_of_int cols in let rows_str = string_of_int rows in
-  let label = (cols_str ^ "x" ^ rows_str) in
-  let box = I.string A.(fg lightgreen ++ bg lightblack) label in
-  center box cols rows
 
 let calculate_tiles_w_h (w, h) =
   let tiles_w = (float_of_int w -. float_of_int left_padding -.
