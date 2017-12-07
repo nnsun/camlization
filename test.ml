687327db994ed8f55acd4315eec94102aa9cf3e3
Ning Ning Sun <nsun200@live.com>
2017-12-07 13:42:33 -0500

    Add tests

--- test.ml
+++ test.ml
@@ -17,6 +17,10 @@ let next = next_turn init
 let player2 = next.players.(1)
 let new_player2 = Player.research_tech player2 Tech.Agriculture
 
+let p3_turn = next_turn next
+
+let p1_again = next_turn (next_turn p3_turn)
+
 let tests = [
   "init_turns" >:: (fun _ -> assert_equal init.player_turns 0);
   "num_players" >:: (fun _ -> assert_equal (Array.length init.players) num_players);
@@ -30,7 +34,11 @@ let tests = [
   "next_curr_player" >:: (fun _ -> assert_equal next.current_player 1);
   "diff_selected" >:: (fun _ -> assert_equal (init.selected_tile <> next.selected_tile) true);
   "available_techs" >:: (fun _ -> assert_equal (available_techs init) [Tech.Agriculture]);
-  "research_ag" >:: (fun _ -> assert_equal (Player.current_tech new_player2) (Some Tech.Agriculture))
+  "research_ag" >:: (fun _ -> assert_equal (Player.current_tech new_player2) (Some Tech.Agriculture));
+  "p3" >:: (fun _ -> assert_equal p3_turn.current_player 2);
+  "p3_turn2" >:: (fun _ -> assert_equal p3_turn.player_turns 2);
+  "p1_again" >:: (fun _ -> assert_equal p1_again.current_player 0);
+  "player_turns" >:: (fun _ -> assert_equal p1_again.player_turns num_players);
 ]
 
 let suite = "test_suite" >::: tests
